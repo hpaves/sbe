ea44e5ea8c24b6dc44a96898ec34ede1bb2680ad
Henri Paves <henri.paves@gmail.com>
2017-03-19 17:45:45 +0200

    input form works, but it's ugly

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
-<form action="index.php" method="POST">
-	<input type="text" name="nimetus" placeholder="Toote nimetus" required><br />
-	<textarea name="kirjeldus" placeholder="Mõni sõna toote kohta"></textarea><br />
-	<input type="number" name="hind" placeholder="€ (ära sente pane)" required><br />
+<form action="mysql_input.php" method="POST">
+	<input type="text" name="Nimetus" placeholder="Toote nimetus" required><br />
+	<textarea name="Kirjeldus" placeholder="Mõni sõna toote kohta"></textarea><br />
+	<input type="Number" name="Hind" placeholder="€ (ära sente pane)" required><br />
 	<button type="submit">Lae üles!</button><br />
 </form>
