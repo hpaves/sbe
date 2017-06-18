461662752f72a445d0e424d3076bce13e9054003
Henri Paves <henri.paves@gmail.com>
2017-06-18 21:24:27 +0300

    muutmise vorm ka nüüd bootstrapi baasil

--- updateform.php
+++ updateform.php
@@ -20,21 +20,23 @@
 
 		        $result = mysqli_query($connect,"SELECT * FROM hp_test WHERE id = '$id'"); /* EXECUTE QUERY AND USE ' ' (apostrophe) IN YOUR VARIABLE */
 
-        		echo "<div class='form'>
-			<form enctype='multipart/form-data' action='?p=updatesubmit' method='POST'>"; /* SUBMIT PAGE ON ITSELF */
+        		echo "<div class='row'>
+        		<div class='col-sm-4'></div>
+				<div class='col-sm-4 col-sm-offset-4'>
+				<form enctype='multipart/form-data' action='?p=updatesubmit' method='POST'>"; /* SUBMIT PAGE ON ITSELF */
 
 			while ($row = mysqli_fetch_array($result)){ /* FETCH ARRAY */
 
 			    $id=mysqli_real_escape_string($connect,$row['id']);
 
 			    echo "<label>Toote nimetus:</label>";
-			    echo "<input class='form' type='text' name='Nimetus' maxlength='255' required value='".htmlspecialchars($row['Nimetus'])."'><br>";
+			    echo "<input class='form-control' type='text' name='Nimetus' maxlength='255' required value='".htmlspecialchars($row['Nimetus'])."'><br>";
 
 			    echo "<label>Kirjeldus: </label><br>";
-			    echo "<textarea class='form' name='Kirjeldus'>".htmlspecialchars($row['Kirjeldus'])."</textarea><br>";
+			    echo "<textarea class='form-control' name='Kirjeldus'>".htmlspecialchars($row['Kirjeldus'])."</textarea><br>";
 
 			    echo "<label>Hind:</label>";
-			    echo "<input class='form' type='number' name='Hind' min='0' required value='".htmlspecialchars($row['Hind'])."'><br><br>";
+			    echo "<input class='form-control' type='number' name='Hind' min='0' required value='".htmlspecialchars($row['Hind'])."'><br><br>";
 
 			    echo "<img class='updateform' src='images/" . htmlspecialchars($row['Pildinimi']) . "' alt='" . htmlspecialchars($row["id"]) . " '><br>";
 
@@ -42,6 +44,7 @@
 
 			    echo "<button class='text-center btn btn-primary' type='submit'>Uuenda kirjet!</button>
 				</form>
+			</div>
 			</div>";
 
 			}
